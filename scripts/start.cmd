b4de2c1d793edfa90f05824b094ec00cdc5c0f6a
maslo-tema <57353944+maslo-tema@users.noreply.github.com>
2021-03-11 23:40:00 +0300

    Update start.cmd

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -2,7 +2,7 @@ cd ..\Valuator\
 start dotnet run --no-build --urls "http://localhost:5001"
 start dotnet run --no-build --urls "http://localhost:5002"
 
-cd..\RankCalculator\
+cd ..\RankCalculator\
 start dotnet run --no-build
 start dotnet run --no-build
 
